7ae9a424b73b989b9b0c0e039c79d07827b286cf
Xiang-Fxx <2775634116@qq.com>
2024-02-14 19:32:52 +0800

    commit helloworld.c file

---
+++ helloworld.c
@@ -0,0 +1,23 @@
+/*
+ * @Author: Xiang-Fxx 2775634116@qq.com
+ * @Date: 2024-02-14 19:10:58
+ * @LastEditors: Xiang-Fxx 2775634116@qq.com
+ * @LastEditTime: 2024-02-14 19:15:40
+ * @FilePath: \HelloWorldTest\helloworld.c
+ * @Description: 
+ * 
+ * Copyright (c) 2024 by ${git_name_email}, All Rights Reserved. 
+ */
+
+#include "stdio.h"
+
+
+
+
+void main(void)
+{
+
+    printf("hello world\n");
+
+    return;
+}
